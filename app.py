f939e5ec4b7ed7654168b8b4b4e694ca5504797d
FEDOROV Dmitry <Dmitry.Fedorov@mazars.ru>
2021-11-12 02:43:47 +0300

    start

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import os
+import pandas as pd
+
+
+
+working_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(working_dir, "data")
+
+df = pd.DataFrame()
+
+for data in os.listdir(data_dir):
+    try:
+        tmp_df = pd.read_csv(os.path.join(data_dir, data), delimiter=";")
+        df = df.append(tmp_df)
+    except Exception as e:
+        print(e)
+
+print(df.head())
 
 
 st.write("Initial commit")
-
-
